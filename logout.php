0fd6d7506d2e027182c11bc302eca9e5ba0a2cd3
hubertpage <63717050+hubertpage@users.noreply.github.com>
2020-04-19 22:12:52 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,8 +1,10 @@
 <?php
-
+// Si quelqu un est connecte
   session_start();
+// On enelve ses caractéristiques
   unset($_SESSION["id"]);
   unset($_SESSION["role"]);
+// On le redirige sur la page index
   header("Location: index.php");
 
  ?>
