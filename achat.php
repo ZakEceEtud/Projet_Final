2bb5d6aaad26b260f3ed8fc4cb83cf1f0cb2b263
ArianeChouette <61501682+ArianeChouette@users.noreply.github.com>
2020-04-19 22:36:04 +0200

    Update achat.php

--- achat.php
+++ achat.php
@@ -1,17 +1,24 @@
 <?php
 
+// Insertion init et header (=barre nav)
   $title = "Achat";
   include("include/init.php");
 
+// Direction page de connexion si non connecté
   if(!isConnect()){
     header("Location: login.php");
     die();
   }
 
-  if(isset($_GET["cat"]) && isset($_GET["type"])){
+// Récupération catégorie et type de vente
+  if(isset($_GET["cat"]) && isset($_GET["type"]))
+  {
+    // Variables
     $show_menu = false;
     $cat = $_GET["cat"];
     $type = $_GET["type"];
+
+    // Requête
     $req = mysqli_query($bdd,
       "SELECT objets.*, utilisateurs.pseudo
 			 FROM objets, utilisateurs
@@ -22,6 +29,7 @@
     $show_menu = true;
   }
 
+// Insertion header (=barre de navigation)
   include("include/header.php");
  ?>
 
@@ -37,11 +45,13 @@
 
               <div class="overlay"></div>  <!--Superposition sur le fond d'écran-->
 
+            <!-- Cadre type de vente Enchères-->
             <h3><strong>Enchères :</strong></h3>
             <br>
             <h4>A vous d'encherir' !</h4>
             <br>
             <ul>
+              <!-- Accès trois catégories FT/BM/VIP-->
               <?php foreach ($CAT as $key => $value): ?>
                 <li><a href="achat.php?type=0&cat=<?= $key ?>" ><?= $value ?></a></li><br>
               <?php endforeach; ?>
@@ -55,11 +65,13 @@
 
               <div class="overlay"></div>  <!--Superposition sur le fond d'écran-->
 
+              <!-- Cadre type de vente MO -->
               <h3><strong>Meilleure Offre :</strong></h3>
               <br>
               <h4>A vous de négocier !</h4>
               <br>
               <ul>
+                <!-- Accès trois catégories FT/BM/VIP-->
                 <?php foreach ($CAT as $key => $value): ?>
                   <li><a href="achat.php?type=1&cat=<?= $key ?>" ><?= $value ?></a></li><br>
                 <?php endforeach; ?>
@@ -73,11 +85,13 @@
 
               <div class="overlay"></div>  <!--Superposition sur le fond d'écran-->
 
+              <!-- Cadre type de vente MO -->
               <h3><strong>Achat Immédiat :</strong></h3>
               <br>
               <h4>Ne vous faites pas doubler !</h4>
               <br>
               <ul>
+                <!-- Accès trois catégories FT/BM/VIP-->
                 <?php foreach ($CAT as $key => $value): ?>
                   <li><a href="achat.php?type=2&cat=<?= $key ?>" ><?= $value ?></a></li><br>
                 <?php endforeach; ?>
@@ -96,6 +110,8 @@
 
        <!--Création d'un bloc de contenu-->
        <div class="container">
+
+         <!-- Changement catégorie et type de vente en fonction du lien suivi-->
          <h2>Bienvenu dans l'espace des <?= $TYPE[$type] ?> de <?= $CAT[$cat] ?>.</h2><br>
          <h4>Vous trouverez ici l'intégralité des bijoux et trésors en vente aux enchères. </h4><br>
          <br><br>
@@ -119,6 +135,7 @@
            </thead>
 
            <tbody>
+             <!-- Remplissage des infos des objets en vente-->
                <?php while ($data = mysqli_fetch_array($req)): ?>
                <tr>
                  <td>IMG</td><!-- TD = changement dee colonne-->
@@ -129,12 +146,15 @@
                  <td><a href="account.php?id=<?= $data["id_vendeur"] ?>"><?= $data["pseudo"] ?></a></td>
                  <td>
                    <?php switch ($type) {
+                     // Si vente Enchères :
                      case 0:
                        echo '<button class="btn"><a href="#">Enchérir</a></button>';
                        break;
+                       // Si vente MO :
                      case 1:
                         echo '<button class="btn"><a href="#">Négocier</a></button>';
                         break;
+                        // Si vente AI :
                      case 2:
                         echo '<button class="btn"><a href="panier.php?add=' . $data["id"] . '">Acheter</a></button>';
                         break;
@@ -153,5 +173,6 @@
     <?php endif; ?>
 
  <?php
+ // Inclusion footer (bas de page)
     include("include/footer.php");
 ?>
