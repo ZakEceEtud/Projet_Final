ad06a506e9ac2476caf09d09cac1cbfb509bec47
ZakEceEtud <63717639+ZakEceEtud@users.noreply.github.com>
2020-04-20 08:56:07 +0200

    Update Panier.php

--- Panier.php
+++ Panier.php
@@ -93,7 +93,7 @@
             <h4> Prix total : <?= $total ?> €</h4><br>
 
             <!--Bouton de validation du panier-->
-            <button class="btn"><a href="Paiement_HTML.html">Valider mon panier</a></button>
+            <button class="btn"><a href="Livraison.php">Valider mon panier</a></button>
           </div>
 
         </div>
